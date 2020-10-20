854cb7795813b5edc061d650ee3e8f9a7b4e9f5b
Stella WL <swl515@york.ac.uk>
2020-10-20 11:32:47 +0100

    plot data, renamed summary statistic

--- analysis.R
+++ analysis.R
@@ -8,5 +8,12 @@ library(tidyverse)
 df %>% group_by(group) %>%
   summarise(mean = mean(x),
             n = length(n),
-            sd = sd(x))
+            sdev = sd(x))
+
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+df %>% ggplot(aes(x = group, y = x)) +
+  geom_boxplot()
+
 
